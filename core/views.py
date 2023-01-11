dafc88906c960f67567113fd51f255fe8037aeab
Brian Nganga <briannganga70@gmail.com>
2023-01-11 16:11:57 +0300

    add more views

--- core/views.py
+++ core/views.py
@@ -25,3 +25,29 @@ def apply_loan(request):
         return redirect('home')
     members = Member.objects.all()
     return render(request, 'apply_loan.html', {'members': members})
+
+def approve_loan(request, loan_id):
+    loan = Loan.objects.get(pk=loan_id)
+    loan.status = 'Approved'
+    loan.save()
+    return redirect('home')
+
+def view_members(request):
+    members = Member.objects.all()
+    return render(request, 'view_members.html', {'members': members})
+
+def view_accounts(request, member_id):
+    member = Member.objects.get(pk=member_id)
+    accounts = Account.objects.filter(member=member)
+    return render(request, 'view_accounts.html', {'accounts': accounts})
+
+def make_savings(request):
+    if request.method == 'POST':
+        member = Member.objects.get(pk=request.POST['member'])
+        savings_amount = request.POST['savings_amount']
+        savings_date = request.POST['savings_date']
+        savings = Savings(member=member, savings_amount=savings_amount, savings_date=savings_date)
+        savings.save()
+        return redirect('home')
+    members = Member.objects.all()
+    return render(request, 'make_savings.html', {'members': members})
